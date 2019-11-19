a0b2ef1e83941252d26ee6a70ad974493272c0b7
James_hket <jamesyau@hket.com>
2019-11-19 12:49:15 +0800

    added travis file and configuration

---
+++ build/precache-manifest.2ada27fdd3ff78c2b721a780356a1809.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fc73c0ff9767e6bc4760f942616de72",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5676f0f42688c9268304",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "6c89c4a3253c592a2ae2",
+    "url": "/static/js/2.a723b85c.chunk.js"
+  },
+  {
+    "revision": "5676f0f42688c9268304",
+    "url": "/static/js/main.d4503fc5.chunk.js"
+  },
+  {
+    "revision": "f1c6019deaec6f4a9d0b",
+    "url": "/static/js/runtime-main.11bc70d3.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
